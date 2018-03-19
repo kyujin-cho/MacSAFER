ae5f028760349e51cbd0439f10337ba20b757b16
Kyujin Cho <thy2134@gmail.com>
2018-03-19 10:42:33 +0900

    Add pip requests package to dependency, update version #

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='MacSAFER',
-    version='1.1',
+    version='1.2',
     url='https://github.com/thy2134/MacSAFER',
     license='GPLv3',
     author='Kyujin Cho',
     author_email='thy2134@gmail.com',
     description='Delete malicious plugins from korean e-commerce, government, online banking websites to make your mac clean.',
     python_requires='>=3',
+    install_requires=['requests'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: End Users/Desktop',
